8052873abe84f47d826098ec3218ff15dfa741d3
Sofia Medina <sofi.k.medina@gmail.com>
2023-07-07 23:52:58 -0300

    adding minstrel
    Signed-off-by: Sofia Medina <sofi.k.medina@gmail.com>

--- src/main/java/org/skm/BraveKnight.java
+++ src/main/java/org/skm/BraveKnight.java
@@ -2,13 +2,18 @@ package org.skm;
 
 public class BraveKnight implements Knight{
     private Quest quest;
+    private Minstrel minstrel;
 
-    public BraveKnight(Quest quest)
+    public BraveKnight(Quest quest,Minstrel minstrel)
     {
         this.quest=quest;
+        this.minstrel=minstrel;
     }
 
     @Override
-    public void embarkOnQuest(){quest.embark();
+    public void embarkOnQuest(){
+        minstrel.singBeforeQuest();
+        quest.embark();
+        minstrel.singAfterQuest();
     }
 }
